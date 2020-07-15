7ee35b8e6ea6031ba07273b7ede2697ed72ba98c
Ali <alikhajeh1@googlemail.com>
2020-07-15 21:19:16 +0430

    Simplify action

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ echo "::set-output name=pull_request_monthly_cost::$pull_request_monthly_cost"
 
 absolute_percent_diff=$(echo "scale=4; $master_monthly_cost / $pull_request_monthly_cost * 100 - 100" | bc | tr -d -)
 
-if (( $(echo "$absolute_percent_diff >= $percentage_threshold" | bc -l) )); then
+if [ $(echo "$absolute_percent_diff >= $percentage_threshold" | bc -l) == 1 ]; then
   jq -Mnc --arg diff "$(git diff --no-color --no-index master_infracost.txt pull_request_infracost.txt | tail -n +3)" '{body: "Master branch monthly cost estimate $master_monthly_cost\nPull request monthly cost estimate $pull_request_monthly_cost\n<details><summary>Infracost diff</summary>\n\n```diff\n\($diff)\n```\n</details>\n"}' | \
   curl -sL -X POST -d @- \
     -H "Content-Type: application/json" \
