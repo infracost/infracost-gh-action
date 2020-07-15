6e24da6ee7109de9680756db1e5c812abfc55d18
Ali <alikhajeh1@googlemail.com>
2020-07-15 19:05:21 +0430

    Simplify action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh -l
 
-echo $TERRAFORM_DIR
+echo "Using TERRAFORM_DIR=$TERRAFORM_DIR"
 
+echo "Running infracost on master branch..."
 master_output=$(infracost --no-color --tfdir /github/workspace/master/$TERRAFORM_DIR)
 echo "$master_output"
 echo "$master_output" > master_infracost.txt
 master_monthly_cost=$(echo $master_output | awk '/OVERALL TOTAL/ { print $NF }')
 echo "::set-output name=master_monthly_cost::$master_monthly_cost"
 
+echo "Running infracost on pull_request..."
 pull_request_output=$(infracost --no-color --tfdir /github/workspace/pull_request/$TERRAFORM_DIR)
 echo "$pull_request_output"
 echo "$pull_request_output" > pull_request_infracost.txt
 pull_request_monthly_cost=$(echo $pull_request_output | awk '/OVERALL TOTAL/ { print $NF }')
 echo "::set-output name=pull_request_monthly_cost::$pull_request_monthly_cost"
 
-if [ $master_monthly_cost -eq $pull_request_monthly_cost]; then
+if [ $master_monthly_cost -eq $pull_request_monthly_cost ]; then
   jq -Mnc --arg diff "$(git diff --no-color --no-index master_infracost.txt pull_request_infracost.txt | tail -n +3)" '{body: "Master branch monthly cost estimate $master_monthly_cost\nPull request monthly cost estimate $pull_request_monthly_cost\n<details><summary>Infracost diff</summary>\n\n```diff\n\($diff)\n```\n</details>\n"}' | \
   curl -sL -X POST -d @- \
     -H "Content-Type: application/json" \
