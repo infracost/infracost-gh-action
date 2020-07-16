f87156abd3613b7cc57037d432c9512e651470bb
Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>
2020-07-16 16:42:28 +0430

    Do not comment on PR if the cost does not change (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ echo "::set-output name=pull_request_monthly_cost::$pull_request_monthly_cost"
 percent_diff=$(echo "scale=4; $pull_request_monthly_cost / $master_monthly_cost  * 100 - 100" | bc)
 absolute_percent_diff=$(echo $percent_diff | tr -d -)
 
-if [ $(echo "$absolute_percent_diff >= $percentage_threshold" | bc -l) == 1 ]; then
+if [ $(echo "$absolute_percent_diff > $percentage_threshold" | bc -l) == 1 ]; then
   change_word="increase"
   if [ $(echo "$percent_diff < 0" | bc -l) == 1 ]; then
     change_word="decrease"
@@ -38,5 +38,5 @@ if [ $(echo "$absolute_percent_diff >= $percentage_threshold" | bc -l) == 1 ]; t
             -H "Authorization: token $GITHUB_TOKEN" \
             "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments" > /dev/null
 else
-  echo "GitHub comment not posted as master branch and pull request diff ($absolute_percent_diff) is less than the percentage threshold ($percentage_threshold)."
+  echo "GitHub comment not posted as master branch and pull request diff ($absolute_percent_diff) is not more than the percentage threshold ($percentage_threshold)."
 fi
